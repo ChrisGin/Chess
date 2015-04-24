a8097b155379caf7dc0ac57b903b5976b30957c4
yarelyc <yarely2011@yahoo.com>
2015-04-23 17:43:49 -0700

    piece.h

---
+++ chessprojectstuff/Bishop.h
@@ -0,0 +1,23 @@
+#ifndef BISHOP_H_INCLUDED
+#define BISHOP_H_INCLUDED
+
+// ********************************************************
+// Class Name:
+//
+/* Description: This class provides
+
+
+*/
+// *******************************************************
+
+#endif // BISHOP_H_INCLUDED
+
+class Rock : public piece{
+
+public:
+
+private:
+
+    bool isValidMove(int moveX, int moveY);
+
+}
---
+++ chessprojectstuff/Rock.h
@@ -0,0 +1,25 @@
+#ifndef ROCK_H_INCLUDED
+#define ROCK_H_INCLUDED
+
+// ********************************************************
+// Class Name: This creates the
+//
+/* Description: This class provides
+
+
+*/
+// *******************************************************
+#include "piece.h"
+
+class Rock : public piece{
+
+public:
+
+private:
+
+    bool validMove(int moveX, int moveY);
+
+}
+
+
+#endif // ROCK_H_INCLUDED
